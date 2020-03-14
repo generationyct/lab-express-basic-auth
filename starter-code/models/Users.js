797afa0a647fa74056d3fe7bf4af660100698203
Devgen <dev@generationyct.nl>
2020-03-14 15:45:54 +0100

    Yes finished the first interation

--- starter-code/models/Users.js
+++ starter-code/models/Users.js
@@ -5,9 +5,9 @@ const uniqueValidator = require('mongoose-unique-validator')
 const userSchema = new mongoose.Schema({
   username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
   email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-  password: {type: String, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: false},
-  hash: String,
-  salt: String
+  password: {type: String, required: [true, "can't be blank"] }
+  // hash: String,
+  // salt: String
 }, {timestamps: true})
 
 userSchema.plugin(uniqueValidator, {message: 'is already taken.'})
@@ -15,3 +15,5 @@ userSchema.plugin(uniqueValidator, {message: 'is already taken.'})
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
+
+// match: [/\S+@\S+\.\S+/, 'is invalid'], index: false
--- starter-code/routes/auth/login.js
+++ starter-code/routes/auth/login.js
@@ -1,10 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../../models/Users");
 
 /* GET user login page */
-router.get('/', (req, res, next) => {
-  console.log('Hit the login page!');
-  res.render('login');
+router.get("/", (req, res, next) => {
+  console.log("Hit the login page!");
+  res.render("login");
 });
+
+router.post("/", (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  bcrypt.hash(password, 10).then(hash => {
+    return User.create({
+      username: username,
+      email: email,
+      password: hash
+    }).then(user => {
+      res.send(user);
+    });
+  });
+});
+
+// res.render('login');
+
+
 
 module.exports = router;
